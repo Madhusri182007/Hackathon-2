150bd8d9e8009499f4cf413b6753725a92173658
Madhusri182007 <madhusri182007@gmail.com>
2024-12-03 20:41:21 +0530

    Update 88Merged_Array.c

--- 88Merged_Array.c
+++ 88Merged_Array.c
@@ -1,16 +1,16 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-    int p1=m-1;
-        int p2=n-1;
-        int i= m+n-1;
-        while(p2>=0)
+    int i=m-1;
+        int j=n-1;
+        int k= m+n-1;
+        while(j>=0)
         {
-            if(p1>=0&&nums1[p1]>nums2[p2])
+            if(i>=0&&nums1[i]>nums2[j])
             {
-                nums1[i--]=nums1[p1--];
+                nums1[k--]=nums1[i--];
             }
             else
             {
-                nums1[i--]=nums2[p2--];
+                nums1[k--]=nums2[j--];
             }
         }
 }
